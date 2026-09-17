254d836cdd7ba876791f5045eefdcecde556ad49
Zahratunnisa <zahralsm540@gmail.com>
2026-09-17 11:36:44 +0700

    improve page design

--- products.php
+++ products.php
@@ -3,34 +3,64 @@
 $products = [
     [
         "id" => 1,
-        "nama" => "Laptop Asus",
-        "kategori" => "Elektronik",
-        "harga" => 8000000,
+        "nama" => "Facial Wash Brightening",
+        "kategori" => "Cleanser",
+        "harga" => 45000,
         "stok" => 10,
-        "deskripsi" => "Laptop untuk kebutuhan kerja"
+        "deskripsi" => "Membersihkan wajah dan membantu mencerahkan kulit."
     ],
+
     [
         "id" => 2,
-        "nama" => "Mouse Logitech",
-        "kategori" => "Aksesoris",
-        "harga" => 250000,
-        "stok" => 5,
-        "deskripsi" => "Mouse wireless"
+        "nama" => "Hydrating Toner",
+        "kategori" => "Toner",
+        "harga" => 55000,
+        "stok" => 8,
+        "deskripsi" => "Melembapkan dan menyegarkan kulit."
     ],
+
     [
         "id" => 3,
-        "nama" => "Keyboard Mechanical",
-        "kategori" => "Aksesoris",
-        "harga" => 350000,
+        "nama" => "Serum Niacinamide",
+        "kategori" => "Serum",
+        "harga" => 85000,
         "stok" => 2,
-        "deskripsi" => "Keyboard mechanical"
+        "deskripsi" => "Membantu merawat kulit dan meratakan warna kulit."
     ],
+
     [
         "id" => 4,
-        "nama" => "Headset Gaming",
-        "kategori" => "Aksesoris",
-        "harga" => 500000,
-        "stok" => 0,
-        "deskripsi" => "Headset untuk gaming"
+        "nama" => "Moisturizer Gel",
+        "kategori" => "Moisturizer",
+        "harga" => 70000,
+        "stok" => 6,
+        "deskripsi" => "Menjaga kelembapan kulit tanpa terasa berat."
+    ],
+
+    [
+        "id" => 5,
+        "nama" => "Sunscreen SPF 50",
+        "kategori" => "Sunscreen",
+        "harga" => 90000,
+        "stok" => 12,
+        "deskripsi" => "Membantu melindungi kulit dari paparan sinar matahari."
+    ],
+
+    [
+        "id" => 6,
+        "nama" => "Lip Tint",
+        "kategori" => "Make Up",
+        "harga" => 65000,
+        "stok" => 1,
+        "deskripsi" => "Memberikan warna pada bibir dengan hasil yang natural."
+    ],
+
+    [
+        "id" => 7,
+        "nama" => "Clay Mask",
+        "kategori" => "Masker",
+        "harga" => 60000,
+        "stok" => 5,
+        "deskripsi" => "Membantu membersihkan pori-pori dan mengangkat kotoran."
     ]
 ];
